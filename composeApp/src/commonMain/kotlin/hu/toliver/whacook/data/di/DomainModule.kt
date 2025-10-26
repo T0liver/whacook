be8da641e46f61e0dad25c2f67cc6e9ca964111d
T0liver <taschnero@edu.bme.hu>
2025-10-26 21:49:29 +0100

    Add the suecase to domain module

--- composeApp/src/commonMain/kotlin/hu/toliver/whacook/data/di/DomainModule.kt
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/data/di/DomainModule.kt
@@ -1,7 +1,8 @@
 package hu.toliver.whacook.data.di
 
+import hu.toliver.whacook.domain.usecase.RecepieGenerationUseCase
 import org.koin.dsl.module
 
 val domainModule = module {
-    factory { /* GenerateRecipeUseCase(get()) */ }
+    factory { RecepieGenerationUseCase(get()) }
 }
