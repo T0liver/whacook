5dd253cf4fff29a05a45b33b8e4efcd05a275bac
T0liver <taschnero@edu.bme.hu>
2025-10-27 16:51:08 +0100

    Add serializable tag to data classes

--- composeApp/src/commonMain/kotlin/hu/toliver/whacook/data/remote/dto/GeminiResponse.kt
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/data/remote/dto/GeminiResponse.kt
@@ -1,14 +1,20 @@
 package hu.toliver.whacook.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class GeminiResponse (
     val id: String,
     val candidates: List<Candidate>
 ) {
     val text: String get() = candidates.firstOrNull()?.content?.parts?.firstOrNull()?.text.orEmpty()
 
+    @Serializable
     data class Candidate(val content: Content?)
 
+    @Serializable
     data class Content(val parts: List<Part>?)
 
+    @Serializable
     data class Part(val text: String?)
 }
