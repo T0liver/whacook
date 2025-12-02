0b598e5acbe1986a6c1ddbb8370f5fe29a81071b
T0liver <taschnero@edu.bme.hu>
2025-12-02 17:15:43 +0100

    Add divider

---
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/components/Divider.kt
@@ -0,0 +1,26 @@
+package hu.toliver.whacook.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Divider(
+    color: Long = 0xFF6C0000,
+    width: Dp = 90.dp,
+    thickness: Dp = 2.dp,
+) {
+    Box(
+        modifier = Modifier.width(width),
+    ) {
+        HorizontalDivider(
+            thickness = thickness,
+            color = Color(color)
+        )
+    }
+}
