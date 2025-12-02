42c3b1055d79b89b74e8d9b5d6b8b6c872d47110
T0liver <taschnero@edu.bme.hu>
2025-12-02 17:18:49 +0100

    Extend subheader

--- composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/components/Subheader.kt
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/components/Subheader.kt
@@ -1,15 +1,27 @@
 package hu.toliver.whacook.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Subheader(
     text: String,
 ) {
-    Text(
-        text = text,
-        style = MaterialTheme.typography.headlineMedium,
-    )
+    Column {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier
+                .padding(top = 16.dp, bottom = 4.dp)
+        )
+        Separator()
+        Spacer(Modifier.height(8.dp))
+    }
 }
