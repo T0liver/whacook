6b4e41510cbb9441205257cb781e369bbf75c2ee
T0liver <taschnero@edu.bme.hu>
2025-12-05 22:08:11 +0100

    Add the right way to initialize koin in App.kt

--- composeApp/src/commonMain/kotlin/hu/toliver/whacook/App.kt
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/App.kt
@@ -3,12 +3,15 @@ package hu.toliver.whacook
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
+import hu.toliver.whacook.data.di.initKoin
 import hu.toliver.whacook.ui.screens.home.HomeScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    initKoin()
+
     MaterialTheme {
         Navigator(HomeScreen())
     }
