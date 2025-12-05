a2c33df197d9fa2bf67aadef09181025e356b68a
T0liver <taschnero@edu.bme.hu>
2025-12-05 23:39:31 +0100

    Remove unused import

--- composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/components/MessageSnackbar.kt
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/components/MessageSnackbar.kt
@@ -1,12 +1,14 @@
 package hu.toliver.whacook.ui.components
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
