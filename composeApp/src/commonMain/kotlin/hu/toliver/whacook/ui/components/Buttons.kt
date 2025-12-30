07d9302acce29a05d050fc9bcaf232291475c283
T0liver <taschnero@edu.bme.hu>
2025-12-30 09:42:59 +0100

    Move BackButton to Buttons.kt

--- composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/components/Buttons.kt
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/components/Buttons.kt
@@ -4,11 +4,18 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import hu.toliver.whacook.ui.theme.LightColors
+import org.jetbrains.compose.resources.painterResource
+import whacook.composeapp.generated.resources.Res
+import whacook.composeapp.generated.resources.back
 
 @Composable
 fun PButton(
@@ -29,5 +36,18 @@ fun PButton(
         shape = RoundedCornerShape(20.dp)
     ) {
         Text(text)
+    }
+}
+
+@Composable
+fun BackButton() {
+    val navigator = LocalNavigator.currentOrThrow
+    IconButton(
+        onClick = { navigator.pop() }
+    ) {
+        Icon(
+            painter = painterResource(Res.drawable.back),
+            contentDescription = "Back"
+        )
     }
 }
