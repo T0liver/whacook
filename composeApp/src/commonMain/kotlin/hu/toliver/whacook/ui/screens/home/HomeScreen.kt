9145b3b0b73448d3c5a68cea0ed01c6ea8bd1eb0
T0liver <taschnero@edu.bme.hu>
2025-12-31 11:13:42 +0100

    Add the components to the homescreen to show off

--- composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/screens/home/HomeScreen.kt
@@ -10,7 +10,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.koinScreenModel
-import hu.toliver.whacook.ui.components.typography.Header
+import hu.toliver.whacook.ui.components.BodyTextUnderline
+import hu.toliver.whacook.ui.components.PButton
+import hu.toliver.whacook.ui.components.TypeBar
+import hu.toliver.whacook.ui.components.Header
+import hu.toliver.whacook.ui.components.RecipeCard
+import hu.toliver.whacook.ui.components.SearchCard
+import hu.toliver.whacook.ui.components.Subheader
 
 class HomeScreen() : Screen {
     @Composable
@@ -33,7 +39,13 @@ private fun HomeScreenContent(
         modifier = Modifier.fillMaxWidth()
     ) {
         Header()
+        Spacer(Modifier.height(8.dp))
+        SearchCard()
         Spacer(Modifier.height(16.dp))
-        PButton("Add Recipe")
+        Subheader("Recent recipes")
+        Spacer(Modifier.height(8.dp))
+        Column(Modifier.fillMaxWidth(0.85f)) {
+            RecipeCard()
+        }
     }
 }
