09d67ce7bac76dbcf30a1e33fea386cbc0f49b07
T0liver <taschnero@edu.bme.hu>
2026-01-06 15:08:55 +0100

    Create Menu

--- composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/screens/menu/MenuScreen.kt
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/screens/menu/MenuScreen.kt
@@ -1,21 +1,19 @@
 package hu.toliver.whacook.ui.screens.menu
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.koinScreenModel
+import hu.toliver.whacook.ui.components.BodyTextSmall
 import hu.toliver.whacook.ui.components.Header
-import hu.toliver.whacook.ui.components.PButton
+import hu.toliver.whacook.ui.components.MenuElement
+import whacook.composeapp.generated.resources.Res
+import whacook.composeapp.generated.resources.about
+import whacook.composeapp.generated.resources.feedback
+import whacook.composeapp.generated.resources.key
 
 class MenuScreen : Screen {
     @Composable
@@ -35,7 +33,14 @@ fun MenuScreenContent(
             modifier = Modifier.fillMaxWidth()
         ) {
             Header("Settings")
-            
+            Spacer(Modifier.height(20.dp))
+            MenuElement("API Key", Res.drawable.key)
+            Spacer(Modifier.height(16.dp))
+            MenuElement("Feedback", Res.drawable.feedback)
+            Spacer(Modifier.height(16.dp))
+            MenuElement("About", Res.drawable.about)
+            Spacer(Modifier.height(16.dp))
+            BodyTextSmall("In the design the icons are provided by icons8.com. Icons by Icons8.")
             Spacer(Modifier.height(100.dp))
         }
     }
