67c58d7386e1680dff9e1079bff1541043e198d9
T0liver <taschnero@edu.bme.hu>
2026-01-06 11:02:24 +0100

    Add recipe vm to view models via factory

--- composeApp/src/commonMain/kotlin/hu/toliver/whacook/data/di/ViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/data/di/ViewModelModule.kt
@@ -2,11 +2,13 @@ package hu.toliver.whacook.data.di
 
 import hu.toliver.whacook.domain.model.Recipe
 import hu.toliver.whacook.ui.screens.home.HomeScreenViewModel
+import hu.toliver.whacook.ui.screens.newrecipe.NewRecipeScreenViewModel
 import hu.toliver.whacook.ui.screens.recipe.RecipeScreenViewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     factory { HomeScreenViewModel(get(), get()) }
+    factory { NewRecipeScreenViewModel() }
 
     factory { (recipe: Recipe) ->
         RecipeScreenViewModel(recipe = recipe)
