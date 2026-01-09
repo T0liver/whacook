e9f4156219d47a64fc65f7e653c43c63fe8e6ee9
T0liver <taschnero@edu.bme.hu>
2026-01-09 20:47:10 +0100

    Create Recipe Preferences Screen

---
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/screens/recipepreferences/RecipePreferencesScreenViewModel.kt
@@ -0,0 +1,27 @@
+package hu.toliver.whacook.ui.screens.recipepreferences
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import cafe.adriel.voyager.core.model.ScreenModel
+
+class RecipePreferencesScreenViewModel : ScreenModel {
+    
+    var state by mutableStateOf(RecipePreferencesState())
+        private set
+    
+    fun updatePreference(value: String) {
+        state = state.copy(preference = value)
+    }
+    
+    fun removePreference() {
+        state = state.copy(preference = "")
+    }
+    
+    fun savePreference() {
+        // TODO: Persist the preference
+        // For now, we simulate a successful save for the demo
+        println("Saving Preference: ${state.preference}")
+        state = state.copy(isSaved = true)
+    }
+}
