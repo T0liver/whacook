fe28af5b095bbd77243c9d8b1194a1dee74b6bd0
T0liver <taschnero@edu.bme.hu>
2026-01-11 16:02:25 +0100

    Add dependecies
    Added multiplatformSettings for api key store
    Added koin-android
    
    Updated kotlin to 2.3.0
    Updated android activity to 1.12.2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -48,6 +48,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.koin.android)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -58,6 +59,9 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+
+            implementation(libs.multiplatformSettings)
+            implementation(libs.multiplatformSettings.coroutines)
 
             implementation(libs.kotlinx.serialization.json)
 
