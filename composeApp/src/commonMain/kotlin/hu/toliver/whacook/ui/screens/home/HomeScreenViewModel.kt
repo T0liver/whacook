aa360b98e26e1f48437cca2ab7fa750f59514d23
T0liver <taschnero@edu.bme.hu>
2026-01-11 21:55:48 +0100

    List the recipes from the database
    We ball!

--- composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/screens/home/HomeScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/hu/toliver/whacook/ui/screens/home/HomeScreenViewModel.kt
@@ -4,20 +4,30 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import hu.toliver.whacook.domain.model.Recipe
 import hu.toliver.whacook.domain.usecase.RecepieGenerationUseCase
 import hu.toliver.whacook.domain.usecase.RecepieUseCase
-import hu.toliver.whacook.domain.model.Recipe
+import kotlinx.coroutines.launch
 
 class HomeScreenViewModel (
     private val recipeUseCase: RecepieUseCase,
-    private val recepieGenerationUseCase: RecepieGenerationUseCase
+    private val recipeGenerationUseCase: RecepieGenerationUseCase
 ) : ScreenModel {
-    var state by mutableStateOf(HomeState(
-        isLoading = true,
-        error = null,
-        showPopUp = false
-    ))
+    var state by mutableStateOf(HomeState.getDefaultState())
         private set
+
+    init {
+        loadRecipes()
+    }
+
+    private fun loadRecipes() {
+        screenModelScope.launch {
+            recipeUseCase.getAllFromDatabase().collect { recipes ->
+                state = state.copy(recipes = recipes)
+            }
+        }
+    }
 
     /**
      * Generate a recipe from the given ingredient list using the injected
@@ -29,7 +39,7 @@ class HomeScreenViewModel (
     suspend fun generateRecipe(ingredients: List<String>): Recipe? {
         try {
             state = state.copy(isLoading = true, error = null)
-            val json = recepieGenerationUseCase.generateRecipe(ingredients)
+            val json = recipeGenerationUseCase.generateRecipe(ingredients)
             val recipe = recipeUseCase.load(json)
             state = state.copy(isLoading = false, error = null)
             return recipe
