f5fece5fcc507735592357f4bf899a74301fb4a3
T0liver <taschnero@edu.bme.hu>
2026-01-12 15:06:45 +0100

    Add Recipes adapter to DatabaseFactory for type conversion

--- composeApp/src/wasmJsMain/kotlin/hu/toliver/whacook/data/local/DatabaseFactory.kt
+++ composeApp/src/wasmJsMain/kotlin/hu/toliver/whacook/data/local/DatabaseFactory.kt
@@ -1,6 +1,8 @@
 package hu.toliver.whacook.data.local
 
+import app.cash.sqldelight.ColumnAdapter
 import app.cash.sqldelight.driver.worker.WebWorkerDriver
+import hu.toliver.whacook.db.Recipes
 import hu.toliver.whacook.db.WebDatabase
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.GlobalScope
@@ -18,10 +20,16 @@ object WebDatabaseFactory {
         // Asynchronously create the database schema
         WebDatabase.Schema.create(driver).await()
 
-        WebDatabase(driver)
+        val recipesAdapter = Recipes.Adapter(
+            ratingAdapter = object : ColumnAdapter<Int, Long> {
+                override fun decode(databaseValue: Long): Int = databaseValue.toInt()
+                override fun encode(value: Int): Long = value.toLong()
+            }
+        )
+
+        WebDatabase(driver, recipesAdapter)
     }
 
     // Suspending provider to ensure initialization is complete before use
     suspend fun getDatabase(): WebDatabase = databaseDeferred.await()
 }
-
